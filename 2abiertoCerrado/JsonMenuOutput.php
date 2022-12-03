87763d5ee63c168c9b8b2b8946f1ae0a8602d4a9
jazmin <correo@example>
2022-12-03 00:04:25 -0500

    actualizado

--- 2abiertoCerrado/JsonMenuOutput.php
+++ 2abiertoCerrado/JsonMenuOutput.php
@@ -3,8 +3,10 @@
 namespace App;
 
 
-class JsonMenuOutput implements MenuOutput {
-    public function output($products){
+class JsonMenuOutput implements MenuOutput 
+{
+    public function output($products)
+    {
         return $menu = json_encode($products);
     }
 }
--- 2abiertoCerrado/Menu.php
+++ 2abiertoCerrado/Menu.php
@@ -4,24 +4,8 @@ namespace App;
 
 class Menu{
 
-    public function view($products, $json= false, $html = false)
+    public function view($products, $output)
     {
-        $menu = '';
-
-        if($json){
-            $menu = json_encode($products);
-        }else{
-            $menu .= "ID\tNombre\t\tPrecio\n";
-            $menu .= str_repeat('=', 30) . "\n";
-            
-            foreach($this->products as $product){
-                $menu .= $product['id'] . "\t"
-                . str_pad($product['name'], 15, '') . "\t"
-                . $product['price'] . "\n";
-            }
-
-        }
-    
-        return $menu;
+        return $output->output($products);
     }
 }
